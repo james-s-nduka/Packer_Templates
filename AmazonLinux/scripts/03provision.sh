5bc05d8f2832025376a6f0b8b9086dc55b631565
JNduka <james.s.nduka@hotmail.com>
2019-10-01 13:01:56 +0100

    feature: build latest am2 image with all features for base synwin

--- AmazonLinux/scripts/03provision.sh
+++ AmazonLinux/scripts/03provision.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+yum update -y 
+
 # Necessary packages
+yum install -y yum-utils device-mapper-persistent-data lvm2 jq
 yum install net-tools -y
+yum install awslogs -y
+yum install git -y
 
 # Install CHEF ver.12.17.44
-yum install wget -y
-wget https://packages.chef.io/files/stable/chef/12.17.44/el/7/chef-12.17.44-1.el7.x86_64.rpm -O /tmp/chef-12.17.44-1.el7.x86_64.rpm
-rpm -ivh /tmp/chef-12.17.44-1.el7.x86_64.rpm
+# yum install wget -y
+# wget https://packages.chef.io/files/stable/chef/12.17.44/el/7/chef-12.17.44-1.el7.x86_64.rpm -O /tmp/chef-12.17.44-1.el7.x86_64.rpm
+# rpm -ivh /tmp/chef-12.17.44-1.el7.x86_64.rpm
 
-# Necessary Packages for Ansible ver.2.1.2.0
+# Install ansible
 yum install epel-release -y
-yum install ansible-2.1.2.0-1.el7 -y
+pip install ansible
+
+# Install Docker
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker -y 
+sudo systemctl start docker && sudo systemctl enable docker
+usermod -aG docker ec2-user
 
 # Cleanup
 yum clean all -y
-rm -rf /tmp/chef-12.17.44-1.el7.x86_64.rpm 
+rm -rf /var/cache/yum
+# rm -rf /tmp/chef-12.17.44-1.el7.x86_64.rpm 
